945cebd7bdc0086ae90cd9101d9a12d85b1383b8
Christian Goeschel Ndjomouo <124460207+christiangoeschel@users.noreply.github.com>
2023-10-09 14:58:14 -0400

    Update aliases-and-functions.sh

--- redhat/centosstream9/alias.sh
+++ redhat/centosstream9/alias.sh
@@ -1,3 +1,13 @@
 # This is will output the exact amount of packages installed on your system
 alias countpkg="rpm -qa | sort -fu | wc -l"
 
+# Checking if for package conflicts
+pkgconflict() {
+    # Printing conflicting package
+    rpm -q --whatconflicts "$1"
+    echo "\n"
+    echo "Conflicting packages:\n"
+    # Checking whether the conflicting package files are installed
+    rpm -q --state $(rpm -q --whatconflicts "$1")
+    
+}
