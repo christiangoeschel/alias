a5bf1ae0ef1a2c2cc6e7a10ab12a32f0ac7667bd
Christian Goeschel Ndjomouo <124460207+christiangoeschel@users.noreply.github.com>
2023-10-10 14:25:38 -0400

    Update functions.sh

--- redhat/centosstream9/functions.sh
+++ redhat/centosstream9/functions.sh
@@ -1,9 +1,22 @@
 # Useful functions for a smoother system administration
 
-# A lot of terminals still do not support colors and therefore it can be a little tedious to manually check 
-# a given items file type. 
+# Checking for package conflicts using rpm
+
+pkgconflict() {
+
+    # Printing conflicting package(s)
+    echo -e 'Checking for conflicting package(s) with "$1"'
+    echo -e 'Conflicting package(s) $(rpm -q --whatconflicts "$1")\nPackage file(s) state:\n'
+
+    # Checking whether the conflicting package(s) file(s) are installed
+    rpm -q --state $(rpm -q --whatconflicts "$1")
+
+}
+
+# A lot of terminals still do not support colors and therefore it can be a little tedious to manually check
+# a given items file type.
 # The -F option for the 'ls' command will append the following symbols to the corresponding file types:
-# 
+#
 # /        directories
 # @        symbolic links
 # *        executables
@@ -13,7 +26,9 @@
 # none     regular file
 function l(){
 
-  echo -e 'Linux file types suffix list:\n"/" - directories, "@" - symbolic links, "*" - executables, "|" - FIFOs (named pipes),
-           "=" - sockets, ">" - doors, no suffix means it is a regular file\n\n'
-  ls -F $1
+        echo -e '[ Linux file types suffix list ]\n\n"/" - directories, "@" - symbolic links, "*" - executables,'
+        echo -e '"|" - FIFOs (named pipes), "=" - sockets, ">" - doors, no suffix means it is a regular file\n\n'
+
+        ls -F $1
+
 }
