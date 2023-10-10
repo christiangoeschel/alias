b952a85a7735c25a19428bdabe3d7613d74f7823
Christian Goeschel Ndjomouo <124460207+christiangoeschel@users.noreply.github.com>
2023-10-10 13:38:05 -0400

    Update functions.sh

--- redhat/centosstream9/functions.sh
+++ redhat/centosstream9/functions.sh
@@ -1,1 +1,17 @@
+# Useful functions for a smoother system administration
 
+# A lot of terminals still do not support colors and therefore it can be a little tedious to manually check 
+# a given items file type. 
+# The -F option for the 'ls' command will append the following symbols to the corresponding file types:
+# 
+# /        directories
+# @        symbolic links
+# *        executables
+# |        FIFOs ( a.k.a named pipes )
+# =        sockets
+# >        doors ( currently only implemented on Solaris )
+# none     regular file
+function l(){
+
+
+}
