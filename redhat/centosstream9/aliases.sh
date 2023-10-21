f648a93c9034ea933c085e3c00345efff1445553
Christian Goeschel Ndjomouo <124460207+christiangoeschel@users.noreply.github.com>
2023-10-21 00:58:34 -0400

    Delete redhat/centosstream9 directory

--- redhat/centosstream9/aliases.sh
+++
@@ -1,5 +0,0 @@
-# This is will output the exact amount of packages installed on your system
-alias countpkg="rpm -qa | sort -fu | wc -l"
-
-# Prints the current Linux distribution full name
-alias osname="echo "$(awk -F 'PRETTY_NAME=' '{ print $2 }' /etc/*-release | grep -vE '^$')""
